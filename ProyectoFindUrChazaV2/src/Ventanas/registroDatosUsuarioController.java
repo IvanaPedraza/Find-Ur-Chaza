ff83183e87c868fb0852fb637dcc7083fcaad4b3
CatGmz <catgomezmo@gmail.com>
2023-05-23 14:43:44 -0500

    actualizacionErrorLibrerias

--- ProyectoFindUrChazaV2/src/Ventanas/registroDatosUsuarioController.java
+++ ProyectoFindUrChazaV2/src/Ventanas/registroDatosUsuarioController.java
@@ -38,12 +38,12 @@ public class registroDatosUsuarioController {
 
     @FXML
     private TextField textFieldTelefono;
-    /*
+    
     @FXML
     private void switchToRegistroChaza() throws IOException{
         App.setRoot("RegistroChaza");
     }
-    */
+   
     
     @FXML
     private void primerEstilo() {
