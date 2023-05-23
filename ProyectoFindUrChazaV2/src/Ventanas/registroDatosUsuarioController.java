29f94385f148d248954a1a38c6cc838ac1f4ffae
Noska2004 <ivanapedra234@gmail.com>
2023-05-22 20:46:46 -0500

    Interfaz registro datos chaza

--- ProyectoFindUrChazaV2/src/Ventanas/registroDatosUsuarioController.java
+++ ProyectoFindUrChazaV2/src/Ventanas/registroDatosUsuarioController.java
@@ -1,10 +1,18 @@
 package Ventanas;
 
+import com.sun.javafx.logging.PlatformLogger.Level;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.lang.System.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class registroDatosUsuarioController {
 
@@ -37,7 +45,13 @@ public class registroDatosUsuarioController {
 
     @FXML
     private TextField textFieldTelefono;
-
+    
+    @FXML
+    private void switchToRegistroChaza() throws IOException{
+        App.setRoot("RegistroChaza");
+    }
+    
+    
     @FXML
     private void primerEstilo() {
         nuevoVendedor.getStylesheets().clear();
@@ -45,5 +59,8 @@ public class registroDatosUsuarioController {
         nuevoCliente.getStylesheets().clear();
         nuevoCliente.getStylesheets().addAll(this.getClass().getResource("../Estilos/Style's.css").toExternalForm());
     }
+        
 
-}
+ }   
+
+
