0033a07ce63853f2c4e53ad71f1e5ddd9e6eb1a5
Catalina <catgomezmo@gmail.com>
2023-05-27 15:38:09 -0500

    ConexionEstructurasInterfaz

--- ProyectoFindUrChazaV2/src/Ventanas/RegistroChazaController.java
+++ ProyectoFindUrChazaV2/src/Ventanas/RegistroChazaController.java
@@ -1,5 +1,8 @@
 package Ventanas;
 
+import EstructurasDeDatos.AVLChaza;
+import Modelo.Chaza;
+import Modelo.Vendedor;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,41 +14,39 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import Ventanas.*;
+import static Ventanas.registroDatosVendedorController.vendedorActual;
+import javafx.scene.input.MouseEvent;
+import javax.swing.JOptionPane;
 
 public class RegistroChazaController {
+    private AVLChaza arbolChaza = new AVLChaza();
+    private Vendedor vendedorChaza = registroDatosVendedorController.vendedorActual;
+    private Chaza chazaActual = new Chaza();
 
     @FXML
     private Button Registrar;
 
-    @FXML
-    private PasswordField PasswordFieldContraseña;
+      @FXML
+    private AnchorPane Panel1;
 
     @FXML
-    private ImageView sushi;
+    private ImageView Wallpaper;
 
     @FXML
-    private ImageView Palillos2;
+    private TextField añadirDescripcion;
 
     @FXML
-    private ImageView PlatilloRegistro;
+    private TextField nombrarChaza;
 
     @FXML
-    private ImageView TituloVendedor1;
+    private TextField ubicacionChaza;
 
     @FXML
-    private ImageView TituloVendedor2;
+    private Button volver;
 
     @FXML
-    private ImageView F2;
-
-    @FXML
-    private TextField TextFieldNombre;
-
-    @FXML
-    private TextField TextFieldUbicacion;
-
-    @FXML
-    private TextField TextFieldDescripcion;
+    private Button volver1;
 
     @FXML
     private void primerEstilo() {
@@ -54,12 +55,26 @@ public class RegistroChazaController {
     }
 
     @FXML
-    private void switchToRegistroDatosVendedor() throws IOException {
-        App.setRoot("registroDatosVendedor");
-    }
-
-    @FXML
     private Button Volver;
 
+    @FXML
+    void registroChaza(MouseEvent event) throws IOException {
+        String nombreChaza = nombrarChaza.getText().trim();
+        String ubicacion = ubicacionChaza.getText().trim();
+        String descripcion = añadirDescripcion.getText().trim();
+
+        if (!nombreChaza.equals("") || !ubicacion.equals("") || !descripcion.equals("")) {
+            chazaActual = new Chaza(nombreChaza, ubicacion, descripcion, vendedorChaza);
+
+            arbolChaza.insert(chazaActual);
+            arbolChaza.postOrderTraversal();
+            App.setRoot("InicioVendedor");
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Debes llenar todos los campos para continuar! :)");
+
+        }
+
+    }
 
 }
